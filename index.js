e3e6b19f0d6ea7d2ffc6b191e58670c6f7c42f8f
JamesNg <njames15@gmail.com>
2022-04-27 17:51:16 -0400

    commit tree b193159a87480066a0f514136b97169384371d84

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(arr) {
+    const findWin = arr.find( win => win.result === 'W');
+    if (findWin) {
+        return findWin.year;
+    } else {
+        return findWin;
+    }
+}
